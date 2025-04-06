d6be71e1545fdcf12ff66633550979497f2af96f
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-04-06 18:36:45 +0200

    UI - Formularz - krok 2.

---
+++ frontend/src/lib/components/diet-form/steps/step-2.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+import { dietState } from "../state.svelte";
+
+import type {
+  Gender,
+  ActivityLevel,
+  Goal,
+  CookingTimePerDay,
+} from "$lib/types";
+
+import { Label } from "$lib/components/ui/label";
+import { Input } from "$lib/components/ui/input";
+import * as Select from "$lib/components/ui/select";
+import Textarea from "$lib/components/ui/textarea/textarea.svelte";
+</script>
+
+<div>
+  <Label for="allergies">Alergie</Label>
+  <Input
+    id="allergies"
+    bind:value={dietState.allergies}
+    placeholder="Opisz swoje alergie (opcjonalne)"
+  />
+</div>
+
+<div>
+  <Label for="intolerances">Nietolerancje</Label>
+  <Input
+    id="intolerances"
+    bind:value={dietState.intolerance}
+    placeholder="Opisz swoje nietolerancje (opcjonalne)"
+  />
+</div>
+
+<div>
+  <Label for="dislikes">Nielubiane produkty</Label>
+  <Textarea
+    id="dislikes"
+    bind:value={dietState.dislikedFoods}
+    placeholder="Opisz swoje nielubiane produkty (opcjonalne)"
+  />
+</div>
+
+<div>
+  <Label for="dislikes">Preferencje kulinarne</Label>
+  <Textarea
+    id="dislikes"
+    bind:value={dietState.dislikedFoods}
+    placeholder="Opisz swoje preferencje kulinarne (opcjonalne)"
+  />
+</div>
+
+<div>
+  <Label for="kitchenEquipment">Doprecyzuj co posiadasz w kuchni</Label>
+  <Textarea
+    id="kitchenEquipment"
+    bind:value={dietState.kitchenEquipment}
+    placeholder="Doprecyzuj co posiadasz w kuchni (opcjonalne)"
+  />
+</div>

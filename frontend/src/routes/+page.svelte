1ed9e12bdb3255af8c77a83637bfbd48b821de20
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-04-06 14:02:09 +0200

    Przygotowanie projektu w SvelteKit z uzyciem shadcn

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="bg-red-500">
+  <h1>Welcome to SvelteKit</h1>
+</div>
+
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<Button>Click me</Button>
